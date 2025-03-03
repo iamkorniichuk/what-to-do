e097e2217d64f6cc4d032ed89048d7e19fbb3642
iamkorniichuk <iamkorniichuk@gmail.com>
2025-03-03 20:18:50 +0300

    feat: filter activities on user configuration

--- app/activities/viewsets.py
+++ app/activities/viewsets.py
@@ -1,3 +1,5 @@
+from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.db.models import F, Q
 from rest_framework import viewsets, parsers, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -13,6 +15,26 @@ class ActivityViewSet(viewsets.ModelViewSet):
     queryset = Activity.objects.prefetch_related("media").all()
     serializer_class = ActivitySerializer
     parser_classes = [parsers.MultiPartParser, parsers.FormParser]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.action == "retrieve":
+            current_user_configuration = self.request.user.configuration
+
+            is_remote_allowed = current_user_configuration.is_remote_allowed
+            start_location = current_user_configuration.start_location
+            allowed_distance = current_user_configuration.allowed_distance
+
+            queryset = (
+                queryset.annotate(distance=Distance(start_location, F("location")))
+                .filter(
+                    Q(location__isnull=is_remote_allowed)
+                    | Q(distance__lte=allowed_distance)
+                )
+                .all()
+            )
+
+        return queryset
 
     def get_permissions(self):
         permissions = super().get_permissions() + [
