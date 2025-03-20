bdbecce0454b2cacadadce95fe78f9a1d66bfdd1
iamkorniichuk <iamkorniichuk@gmail.com>
2025-03-20 08:49:10 +0300

    refactor: simplify signup view

--- app/authentication/serializers.py
+++ app/authentication/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 User = get_user_model()
@@ -9,9 +10,23 @@ User = get_user_model()
 class SignUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("pk", "username", "password")
+        fields = ("pk", "username", "password", "refresh", "access")
         read_only_fields = ("pk",)
-        extra_kwargs = {"password": {"write_only": True}}
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "username": {"write_only": True},
+        }
+
+    refresh = serializers.SerializerMethodField()
+    access = serializers.SerializerMethodField()
+
+    def get_refresh(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token)
+
+    def get_access(self, obj):
+        token = RefreshToken.for_user(obj).access_token
+        return str(token)
 
     def validate_password(self, value):
         user = self.instance
--- app/authentication/views.py
+++ app/authentication/views.py
@@ -1,22 +1,13 @@
 from django.contrib.auth import get_user_model
 from rest_framework.generics import CreateAPIView
 
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
 from .serializers import SignUpSerializer
 
 
 User = get_user_model()
 
 
-class SignUpView(CreateAPIView, TokenObtainPairView):
+class SignUpView(CreateAPIView):
+    serializer_class = SignUpSerializer
     queryset = User.objects.all()
     permission_classes = ()
-
-    def post(self, request, *args, **kwargs):
-        self.serializer_class = SignUpSerializer
-        super(CreateAPIView, self).create(request, *args, **kwargs)
-
-        self.serializer_class = TokenObtainPairSerializer
-        return super(TokenObtainPairView, self).post(request, *args, **kwargs)
